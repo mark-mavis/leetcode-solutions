96999cf0d87cae8597be1ff832b72d25687ac6de
Mark Mavis <mmavis@uw.edu>
2022-05-16 00:31:07 -0700

    Add Merge Two Sorted Linked Lists

---
+++ LeetCode/Node.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <string>
+
+class Node {
+public:
+	int data;
+	Node* next;
+
+	// Default constructor
+	Node()
+	{
+		data = 0;
+		next = NULL;
+	}
+
+	// Parameterised Constructor
+	Node(int data)
+	{
+		this->data = data;
+		this->next = NULL;
+	}
+};
