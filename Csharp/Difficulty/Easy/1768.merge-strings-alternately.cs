85e9ce46b2c1e3e9c57bbf467f02bd41bbe1edc7
markmavis <mark.mavis+github@outlook.com>
2023-09-27 19:38:33 -0700

    Add MergeAlternately

---
+++ Csharp/Difficulty/Easy/1768.merge-strings-alternately.cs
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode id=1768 lang=csharp
+ *
+ * [1768] Merge Strings Alternately
+ */
+
+// @lc code=start
+public partial class Easy {
+    public class MergeAlternately{
+        public static void Run(){
+
+           string result = Solution("abc", "pqr");
+           Console.WriteLine(result);
+        }
+        
+        public static string Solution(string word1, string word2){
+
+            return new string;
+        }
+    }
+}
+// @lc code=end
+
--- Csharp/Difficulty/Easy/EasyController.cs
+++ Csharp/Difficulty/Easy/EasyController.cs
@@ -2,37 +2,38 @@
 public class EasyController{
     public static void Run(){
         
-        Easy.TwoSum.Run();
-        Easy.IsPalindrome.Run();
-        Easy.RemoveDuplicates.Run();
-        Easy.RemoveElement.Run();
-        Easy.SearchInsertPosition.Run();
-        Easy.PlusOne.Run();
-        Easy.MergeSortedArray.Run();
-        Easy.ConvertSortedArrayToBinaryTree.Run();
-        Easy.PascalsTriangleI.Run();
-        Easy.PascalsTriangleII.Run();
-        Easy.BestTimeToBuyAndSellStock.Run();
-        Easy.SingleNumber.Run();
-        Easy.MajorityElement.Run();
-        Easy.ContainsDuplicate1.Run();
-        Easy.ContainsDuplicate2.Run();
-        Easy.SummaryRanges.Run();
-        Easy.MissingNumber.RuN();
-        Easy.MoveZeros.Run();
+        // Easy.TwoSum.Run();
+        // Easy.IsPalindrome.Run();
+        // Easy.RemoveDuplicates.Run();
+        // Easy.RemoveElement.Run();
+        // Easy.SearchInsertPosition.Run();
+        // Easy.PlusOne.Run();
+        // Easy.MergeSortedArray.Run();
+        // Easy.ConvertSortedArrayToBinaryTree.Run();
+        // Easy.PascalsTriangleI.Run();
+        // Easy.PascalsTriangleII.Run();
+        // Easy.BestTimeToBuyAndSellStock.Run();
+        // Easy.SingleNumber.Run();
+        // Easy.MajorityElement.Run();
+        // Easy.ContainsDuplicate1.Run();
+        // Easy.ContainsDuplicate2.Run();
+        // Easy.SummaryRanges.Run();
+        // Easy.MissingNumber.RuN();
+        // Easy.MoveZeros.Run();
         
-        Easy.IntersectionOfTwoArrays.Run();
-        Easy.IntersectionOfTwoArraysII.Run();
-        Easy.ThirdMaximumNumber.Run();
-        Easy.FindAllNumbersDisappearedInAnArray.Run();
-        Easy.IslandPerimeter.Run();
-        Easy.MaxConsecutiveOnes.Run();
-        Easy.TeemoAttacking.Run();
-        Easy.NextGreaterElementI.Run();
-        Easy.KeyboardRow.Run();
-        Easy.RelativeRanks.Run();
-        Easy.ArrayPartition.Run();
-        Easy.ReshapeTheMatrix.Run();
+        // Easy.IntersectionOfTwoArrays.Run();
+        // Easy.IntersectionOfTwoArraysII.Run();
+        // Easy.ThirdMaximumNumber.Run();
+        // Easy.FindAllNumbersDisappearedInAnArray.Run();
+        // Easy.IslandPerimeter.Run();
+        // Easy.MaxConsecutiveOnes.Run();
+        // Easy.TeemoAttacking.Run();
+        // Easy.NextGreaterElementI.Run();
+        // Easy.KeyboardRow.Run();
+        // Easy.RelativeRanks.Run();
+        // Easy.ArrayPartition.Run();
+        // Easy.ReshapeTheMatrix.Run();
+        
 
 
         Console.ReadLine();
